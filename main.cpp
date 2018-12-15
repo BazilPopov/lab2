17b26e5f7c041b52848e372dc3bea9debf7466a8
BazilPopov <45051063+BazilPopov@users.noreply.github.com>
2018-12-15 19:22:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,25 +9,25 @@ using namespace std;
 struct stek
 {
     int value;
-    struct stek *next;                      // указатель на следующий элемент списка (стека)
+    struct stek *next;                
 };
  
 void push(stek* &NEXT, const int VALUE)
 {
-    stek *MyStack = new stek;               // объявляем новую динамическую переменную типа stek
-    MyStack->value = VALUE;                 // записываем значение, которое помещается в стек
-    MyStack->next = NEXT;                   // связываем новый элемент стека с предыдущим
-    NEXT = MyStack;                         // новый элемент стека становится его вершиной
+    stek *MyStack = new stek;              
+    MyStack->value = VALUE;                
+    MyStack->next = NEXT;               
+    NEXT = MyStack;                      
 }
  
 int pop(stek* &NEXT)
 {
-    char temp = NEXT->value;                 // извлекаем в переменную temp значение в вершине стека
-    stek *MyStack = NEXT;                   // запоминаем указатель на вершину стека, чтобы затем
-                                            // освободить выделенную под него память
-    NEXT = NEXT->next;                      // вершиной становится предшествующий top элемент
-    delete MyStack;                         // освобождаем память, тем самым удалили вершину
-    return temp;                            // возвращаем значение, которое было в вершине
+    char temp = NEXT->value;              
+    stek *MyStack = NEXT;                  
+                                           
+    NEXT = NEXT->next;                   
+    delete MyStack;                      
+    return temp;                          
 }
  
 int main()
